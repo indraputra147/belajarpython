09445305098a1d310461ed33ab153e4e033f9909
Indra Putra <32187395+indraputra147@users.noreply.github.com>
2019-07-24 12:45:55 +0700

    Add files via upload

---
+++ Distanceunit15.py
@@ -0,0 +1,10 @@
+f = float(input("Input a measurement in feet : "))
+
+i = f*12
+y = f/3
+m = f*0.000189394
+
+print("Do conversion")
+print("in Inches : %3.2f" % i)
+print("in Yards  : %3.2f" % y)
+print("in Miles  : %3.2f" % m)
